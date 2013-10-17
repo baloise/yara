7824dc8119a9d37b43bdcb1669868ffcc45e5904
Matthias Cullmann <matthias.cullmann@baloise.ch>
2013-10-17 10:33:23 +0200

    Removed unused imports

--- src/main/java/ch/basler/fecru/rest/Root.java
+++ src/main/java/ch/basler/fecru/rest/Root.java
@@ -2,15 +2,11 @@ package ch.basler.fecru.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.atlassian.fisheye.spi.admin.data.GitRepositoryData;
-import com.atlassian.fisheye.spi.admin.services.RepositoryAdminService;
 import com.atlassian.fisheye.spi.admin.services.RepositoryConfigException;
-import com.atlassian.fisheye.spi.services.RepositoryService;
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
 
 
